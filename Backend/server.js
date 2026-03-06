b7944c3af358ba9cca258ec3a762bca1358a273a
SamarthRC <choudharysamarth816@gmail.com>
2026-03-06 15:11:45 +0530

    Initial Commit

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const PORT = process.env.PORT || 5001;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const moodREST_API = require("./routes/mood");
+app.use("/mood", moodREST_API);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
